9886f5c49d846783fe07f64229b126fa7e83d90b
LUXTACO <tacomastabusiness@gmail.com>
2024-11-19 19:35:50 -0600

    Add get_client_data method and ClientData struct
    The IPC class has been updated with a new method `get_client_data()`.
    A new `ClientData` struct has been introduced, containing various
    static members to hold client-related data such as Buffering, AudioTime,
    ReplayTime, SpectatingID, Score, Mode, SkipCalculations, BeatmapChecksum,
    BeatmapId, DimLevel, MenuTime, AudioPlaying, LLastAction, LNextScoreSync,
    LReplayMode, LReplayStreaming, LReplayFrame, and LReplayToEnd.

--- main/src/sdk/ipc/ipc.h
+++ main/src/sdk/ipc/ipc.h
@@ -5,6 +5,29 @@ class IPC {
 public:
 	IPC() : logger(0, false) { return; }
 	IPC(Logger sdkLogger, HANDLE processHandle);
+
+	ClientData get_client_data();
 private:
 	Logger logger;
+};
+
+struct ClientData {
+	static bool Buffering;
+	static int AudioTime;
+    static int ReplayTime;
+    static int SpectatingID;
+    static int Score;
+    static int Mode;
+    static bool SkipCalculations;
+    static std::string BeatmapChecksum;
+    static int BeatmapId;
+    static int DimLevel;
+    static int MenuTime;
+    static bool AudioPlaying;
+    static int LLastAction;
+    static int LNextScoreSync;
+    static bool LReplayMode;
+    static bool LReplayStreaming;
+    static int LReplayFrame;
+    static bool LReplayToEnd;
 };
