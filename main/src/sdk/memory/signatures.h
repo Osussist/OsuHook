07246ae71c2d7c1199a4a06f9c9e9c4a56db9ab4
LUXTACO <tacomastabusiness@gmail.com>
2024-11-27 02:10:34 -0600

    Add Signature and Signatures classes with initial members
    Added <string> header for std::string support. Introduced the
    Signature class with pattern and offset members, and a
    constructor for initialization. Created the Signatures class
    with static Signature instances: AudioTime and BeatmapChecksum.
    AudioTime is initialized with a specific pattern and offset,
    while BeatmapChecksum has an empty pattern and default offset.

--- main/src/sdk/memory/signatures.h
+++ main/src/sdk/memory/signatures.h
@@ -1,2 +1,18 @@
 #pragma once
+#include <string>
 
+class Signature {
+public:
+	Signature(std::string pattern, int offset = 0x0) {
+		this->pattern = pattern;
+		this->offset = offset;
+	}
+	std::string pattern = "";
+	int offset = 0x0;
+};
+
+class Signatures {
+public:
+	inline static Signature AudioTime = Signature("DB 5C 24 34 8B 44 24 34", 0x2);
+	inline static Signature BeatmapChecksum = Signature("", 0x0); // I dont think its possible to get this signature
+};
