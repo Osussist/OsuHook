e3bfdc5ff862861113d2b4f7db3cb642fca0909d
LUXTACO <tacomastabusiness@gmail.com>
2024-12-02 17:21:32 -0600

    Add new members and method to Memory class; rename variables
    The Memory class has been updated to include new member variables
    baseAddr and audioTimeAddr. A new method pattern_scan(Signature
    signature) has been added to the class. The inline static Signature
    variables baseAddr and audioTimeAddr have been renamed to baseSig
    and audioTimeSig, respectively. Comments have been updated to
    reflect these changes.

--- main/src/sdk/memory/memory.h
+++ main/src/sdk/memory/memory.h
@@ -6,14 +6,18 @@ class Memory {
 public:
 	Memory() : logger(0, false) { return; }
 	Memory(Logger sdkLogger, HANDLE processHandle);
+
+	uintptr_t pattern_scan(Signature signature);
+
+	uintptr_t baseAddr;
+	uintptr_t audioTimeAddr;
 private:
 	Logger logger;
 	HANDLE processHandle;
 
-	// Signatures for memory addresses
-	inline static Signature baseAddr = Signature("F8 01 74 04 83 65", 0x0);
-	inline static Signature audioTimeAddr = Signature("DB 5C 24 34 8B 44 24 34", 0x2);
+	inline static Signature baseSig = Signature("F8 01 74 04 83 65", 0x0);
+	inline static Signature audioTimeSig = Signature("DB 5C 24 34 8B 44 24 34", 0x2);
 };
 
-// [baseAddr - 0xc] -> read_ptr = beatmapAddr
+// [baseSig - 0xc] -> read_ptr = beatmapAddr
 // [beatmapAddr + 0x6c] -> read_int -> read_string = beatmapChecksum
